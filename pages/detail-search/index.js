0c41399610a12c8a10917f08b21e710ffdcb4b54
Marshall Chan <marshallchan666@outlook.com>
2022-08-20 23:01:58 +0800

    feat: highlight color for the search keyword with rich-text

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -2,6 +2,7 @@
 import {getHotSearch,getSuggestSearch} from '../../services/api_search'
 import debounce from '../../utils/debounce'
 
+// * 防抖 性能优化
 const debounceGetSearchSuggest = debounce(getSuggestSearch,300)
 
 Page({
@@ -12,7 +13,8 @@ Page({
   data: {
     hotKeywords:[],
     suggestSongs:[],
-    searchValue:''
+    searchValue:'',
+    suggestSongsNodes:[]
   },
 
   /**
@@ -45,8 +47,51 @@ Page({
       searchValue
     })
     debounceGetSearchSuggest(searchValue).then(res=>{
+      const suggestSongs = res.result.allMatch
       this.setData({
-        suggestSongs:res.result.allMatch
+        suggestSongs
+      })
+
+      //  转成nodes节点
+      const suggestKeywords= suggestSongs.map(item => item.keyword)
+      const suggestSongsNodes = []
+      for (const keyword of suggestKeywords) {
+        const nodes = []
+        if (keyword.startsWith(searchValue.toUpperCase())) {
+          // * 字符串切割 因为是匹配开头
+          const key1 = keyword.slice(0,searchValue.length)
+          const key2 = keyword.slice(searchValue.length)
+
+          const node1 = {
+            name:'span',
+            attrs:{
+              style:"color:#26ce8a"
+            },
+            children:[{type:"text", text:key1}]
+          }
+          nodes.push(node1)
+          const node2 = {
+            name:'span',
+            attrs:{
+              style:"color:#000000"
+            },
+            children:[{type:"text", text:key2}]
+          }
+          nodes.push(node2)
+        } else {
+          const node3 = {
+            name:'span',
+            attrs:{
+              style:"color:#000000"
+            },
+            children:[{type:"text", text:keyword}]
+          }
+          nodes.push(node3)
+        }
+        suggestSongsNodes.push(nodes)
+      }
+      this.setData({
+        suggestSongsNodes
       })
     })
   }
