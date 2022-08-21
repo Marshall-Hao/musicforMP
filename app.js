478acb3ac50a9b11266112db6e289cc16167c31b
Marshall Chan <marshallchan666@outlook.com>
2022-08-22 01:04:55 +0800

    feat: layout for the music player with flex and mode for image

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ App({
   },
   onLaunch() {
     const info = wx.getSystemInfoSync() 
-    console.log(info)
+    // console.log(info)
     // * 因为不用响应书渲染页面 直接赋值就好
     this.globalData.screenWidth= info.screenWidth
     this.globalData.screenHeight= info.screenHeight
